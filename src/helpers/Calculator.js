0bfa59fdaed25542e8efb580414fb10fd6d9928f
Yoann Jean <yoann.jean@fullsix.com>
2018-05-15 13:56:32 -0400

    feat: Apply ratio of unit when evaluating recipe cost

--- src/helpers/Calculator.js
+++ src/helpers/Calculator.js
@@ -1,5 +1,9 @@
-export function evaluateIngredientPrice(ingredient, quantity) {
-  return quantity * ingredient.price;
+export function evaluateIngredientPrice(
+  ingredient,
+  quantity,
+  unit = ingredient.unit
+) {
+  return scaledQuantity(quantity, unit, ingredient.unit) * ingredient.price;
 }
 
 export function evaluateRecipePrice(recipe, ingredients) {
@@ -13,7 +17,32 @@ export function evaluateRecipePrice(recipe, ingredients) {
     })
     .reduce(
       (sum, component) =>
-        sum + evaluateIngredientPrice(component.ingredient, component.quantity),
+        sum +
+        evaluateIngredientPrice(
+          component.ingredient,
+          component.quantity,
+          component.unit
+        ),
       0
     );
+}
+
+const GRAM_RATIOS = {
+  gr: 1,
+  lbs: 0.0022,
+  ml: 0.96,
+  l: 0.00096,
+  oz: 0.035,
+  tsp: 0.203,
+  tbsp: 0.0676
+};
+
+function scaledQuantity(quantity, fromUnit, toUnit) {
+  const ratio = GRAM_RATIOS[fromUnit] || 1;
+  if (ratio === 1) {
+    return quantity;
+  }
+  const inGram = quantity * 1 / ratio;
+
+  return inGram * GRAM_RATIOS[toUnit];
 }

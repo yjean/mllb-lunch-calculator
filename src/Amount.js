caa9c8b8860ed2ee0da12a3c6b9bc69bbbfbb9c0
Yoann Jean <yoann.jean@fullsix.com>
2018-05-15 13:55:52 -0400

    fix: Display amount in US format

--- src/Amount.js
+++ src/Amount.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import numeral from 'numeral';
 
-const Amount = ({ amount }) => <span>{numeral(amount).format('$0.00')}</span>;
+const Amount = ({ amount }) => <span>{numeral(amount).format('$0,0.00')}</span>;
 
 export default Amount;

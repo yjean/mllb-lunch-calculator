c6a4d489f23934ff57b91abcf3f315ba8e7e5d55
Yoann Jean <yoann.jean@fullsix.com>
2018-05-15 21:14:58 -0400

    imp: skip 1 ratio for unit conversion

--- src/helpers/Calculator.js
+++ src/helpers/Calculator.js
@@ -28,19 +28,17 @@ export function evaluateRecipePrice(recipe, ingredients) {
 }
 
 const GRAM_RATIOS = {
-  gr: 1,
   lbs: 0.0022,
   ml: 0.96,
   l: 0.00096,
   oz: 0.035,
   tsp: 0.203,
-  tbsp: 0.0676,
-  unit: 1
+  tbsp: 0.0676
 };
 
 function scaledQuantity(quantity, fromUnit, toUnit) {
   const ratio = GRAM_RATIOS[fromUnit] || 1;
   const inGram = quantity * 1 / ratio;
 
-  return inGram * GRAM_RATIOS[toUnit];
+  return inGram * (GRAM_RATIOS[toUnit] || 1);
 }

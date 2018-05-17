f729b693f030274159a2db3fbfc47694fa436b2a
Yoann Jean <yoann.jean@fullsix.com>
2018-05-16 21:42:11 -0400

    fix: Show price/unit only when set

--- src/components/IngredientsList.js
+++ src/components/IngredientsList.js
@@ -2,20 +2,30 @@ import { Button, ListGroup, ListGroupItem } from 'reactstrap';
 
 import React from 'react';
 
-const Ingredient = ({ ingredient, removeIngredient }) => (
-  <ListGroupItem>
-    <div className="float-right">
-      <Button
-        size="sm"
-        color="danger"
-        onClick={() => removeIngredient(ingredient)}
-      >
-        delete
-      </Button>
-    </div>
-    {ingredient.name} ${ingredient.price}/{ingredient.unit}{' '}
-  </ListGroupItem>
-);
+const Ingredient = ({ ingredient, removeIngredient }) => {
+  const hasPrice = ingredient.price > 0;
+  const hasUnit = ingredient.unit !== '';
+
+  return (
+    <ListGroupItem>
+      <div className="float-right">
+        <Button
+          size="sm"
+          color="danger"
+          onClick={() => removeIngredient(ingredient)}
+        >
+          delete
+        </Button>
+      </div>
+      {ingredient.name}{' '}
+      {(hasPrice || hasUnit) && (
+        <span>
+          ${ingredient.price}/{ingredient.unit}
+        </span>
+      )}
+    </ListGroupItem>
+  );
+};
 
 const IngredientsList = ({ ingredients, removeIngredient }) => (
   <ListGroup>
